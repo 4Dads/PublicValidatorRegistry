00d48a92f35217f28389868288d65ffc4d4249bf
4dadsFTSO <support@4dads.io>
2023-11-01 11:33:00 -0500

    mock call, replace/encode values returned

--- frontendmock/script.js
+++ frontendmock/script.js
@@ -1,14 +1,36 @@
+const web3 = new Web3('https://coston-api.flare.network/ext/C/rpc');
+const contractAddress = '0x32580c46242F7C3D6AF86DdF10D82f2Af32cf558'; 
+
+const contractABI =[
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getAllProviderInformation",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
 function minifyJSON() {
-    const address = document.getElementById('address').value;
-    const name = document.getElementById('name').value;
-    const nodeID = document.getElementById('nodeID').value;
-    const url = document.getElementById('url').value;
-    const logourl = document.getElementById('logourl').value;
+    const address = document.getElementById('address').value.replace(/"/g,'&quot;').replace(/'/g,'&#39;');
+    const name = encodeURIComponent(document.getElementById('name').value.replace(/"/g,'&quot;').replace(/'/g,'&#39;'));
+    const nodeID = document.getElementById('nodeID').value; //todo: verify byte20, length = 40
+    const url = encodeURIComponent(document.getElementById('url').value.replace(/"/g,'&quot;').replace(/'/g,'&#39;'));
+    const logourl = encodeURIComponent(document.getElementById('logourl').value.replace(/"/g,'&quot;').replace(/'/g,'&#39;'));
   
-    const data = {
+    let data = {
       address: address,
       name: name,
-      nodeID: [nodeID],
+      nodeID: [nodeID], 
       url: url,
       logourl: logourl
     };
@@ -30,3 +52,20 @@ function minifyJSON() {
         alert('Failed to copy minified JSON. Please copy it manually.');
       });
   }
+
+async function getAllProviderInformation() {
+    try {
+        const result = await contract.methods.getAllProviderInformation().call();
+        const data = result;
+        //const nodes = result[1];
+
+        console.log('Return:', data);
+        //console.log('Nodes:', nodes);
+    } catch (error) {
+        //console.error('Error:', error);
+        if (error.message) {
+            console.error('Error Message:', error.message);
+          }
+    }
+}
+
